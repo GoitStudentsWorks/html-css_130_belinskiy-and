deeeb457c6a39040e58363ee13cf2f9678a04879
Andrew <belinskiy.and@gmail.com>
2026-06-11 14:36:38 +0300

    add burger-button action

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,2 +1,11 @@
-// remove this code
-console.log('mobile-menu');
+const openBtn = document.getElementById('burger-button');
+const closeBtn = document.getElementById('close-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+openBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('is-open');
+});
